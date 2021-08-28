a757825685c91d1cc2d012f64dc37ce2dbe66638
Karla Goo <karla_goo@yahoo.com>
2021-08-27 22:00:32 -0500

    added inquirer prompt

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const db = require ('./db/connection');
+const inquirer = require('inquirer');
 
+function startPrompt(){
+    inquirer.prompt ([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "initialPrompt",
+            choices: ["View All Departments","Add a Department", "View All Roles", "Add a Role", "View All Employees", "Add an Employee", "Update an Employee Role"]
+        }
+    ])
+
+}
 //Inquirer to list out options: view all employees, view all departments, etc.
-//Depedning on which option user chooses
+//Depending on which option user chooses
 //call  and execute the appropriate function
 //meaning for each one of the options, you need to create a function for each of them.
 
